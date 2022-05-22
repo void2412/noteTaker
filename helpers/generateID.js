443b15a1ca52b8d957e256ce472d75a05e94510b
Chi Hieu Nguyen <void2412@gmail.com>
2022-05-22 20:35:15 +0930

    added comment for generateID

--- helpers/generateID.js
+++ helpers/generateID.js
@@ -1,11 +1,14 @@
 const fs = require('fs')
 const crypto = require('crypto').webcrypto
+
+// generate 32-bit non-negative integer, maximum value is 4294967295
 function generateID(){
 	data = JSON.parse(fs.readFileSync('./db/db.json'))
 	let idArray = []
 	data.forEach(element => {
 		idArray.push(element.id)
 	})
+
 
 	var randomID = new Uint32Array(1)
 	crypto.getRandomValues(randomID)
