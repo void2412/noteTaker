301098c7429a804949927e2e76d85d9735ca9a01
Chi Hieu Nguyen <void2412@gmail.com>
2022-05-22 19:51:41 +0930

    minor fix on note.js and server.js

---
+++ routes/notes.js
@@ -0,0 +1,2 @@
+const fs = require('fs')
+const notes = require('express').Router()
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-// app.use('/api', api)
+app.use('/api', api)
 app.use(express.static('public'))
 
 
